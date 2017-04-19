084777180d4f967ed2c20707d9a43d7904c9631b
Tawny <tony.malto.simon@gmail.com>
2017-04-19 16:35:34 +0200

    itération 5

--- app/routes.php
+++ app/routes.php
@@ -8,9 +8,6 @@
 
 $app->get('/', function() use($app) {
     $articles = $app['dao.article']->findAll();
+    return $app['twig']->render('index.html.twig', array('articles' => $articles));
 
-    ob_start();             // start buffering HTML output
-    require '../views/view.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
 });
